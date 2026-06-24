8f25970f0058fa3d7fb45a31951acaa2c60d96c5
tadsaporn09 <467130@wsk.ac.th>
2026-06-24 08:38:21 +0000

    program02.py

---
+++ program02.py
@@ -0,0 +1,9 @@
+print("โปรแกรมโปรแกรมคำนวณพื้นที่และเส้นรอบรูปสี่เหลี่ยมผืนผ้า")
+print("------------------------------------")
+width=float(input("กรอกความกว้าง: "))
+length=float(input("กรอกความยาว: "))
+area=width * length
+perimeter=2 * (width + length)
+print("พื้นที่ =", area)
+print("เส้นรอบรูป =", perimeter)
+print("Author by Tadsaporn")
