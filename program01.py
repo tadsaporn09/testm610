d637d8d3e6ffdb58e3e823d42113bd8b2de23801
tadsaporn09 <467130@wsk.ac.th>
2026-06-24 07:54:44 +0000

    program01.py

---
+++ program01.py
@@ -0,0 +1,7 @@
+print("Hello word")
+print("----------")
+num1=float(input("enter number1:"))
+num2=float(input("enter number2:"))
+sum=num1+num2
+print("total is:",sum)
+print("Author by Tadsaporn")
